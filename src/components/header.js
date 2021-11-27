c21367fdc8deacecb6012f967bed1b9d36ad0acf
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-11-26 20:05:17 -0600

    We create our header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql, useStaticQuery } from 'gatsby';
 
 import { Navegacion } from './navegacion';
 
 import { ContainerHeader } from '../styles/CommonComponents';
 
 export const Header = () => {
+
+    const { logo } = useStaticQuery( graphql`
+        query {
+            logo: file( relativePath: { eq: "logo.svg"} ) {
+                publicURL
+            }
+        }
+    `);
+
     return (
         <ContainerHeader>
             <div>
                 <Link to={ '/' }>
-                    Bienes Raices
+                    <img src={ logo.publicURL } alt='Logotipo Bienes Raices'/>
                 </Link>
 
                 <Navegacion />
