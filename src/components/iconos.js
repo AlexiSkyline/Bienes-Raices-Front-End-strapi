7a9927e566bc78dc150c4c093d2a13b95b9942ff
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-11-27 21:46:18 -0600

    we create the Iconos component

---
+++ src/components/iconos.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import { ListadoIconos } from '../styles/CommonComponents';
+
+export const Iconos = ({ wc, estacionamiento, habitaciones }) => {
+
+    const { iconos } = useStaticQuery(graphql`
+        query {
+            iconos: allFile( filter: { relativeDirectory: { eq: "iconos" } } ) {
+                edges {
+                    node {
+                        id
+                        publicURL
+                    }
+                }
+            }
+        }
+    `);
+
+    const imagenesIconos = iconos.edges;
+
+    return (
+        <ListadoIconos>
+            <li> 
+               <img src={ imagenesIconos[2].node.publicURL } alt='icono wc'/>
+                <p>{ wc }</p>
+            </li>
+            <li> 
+               <img src={ imagenesIconos[1].node.publicURL } alt='icono estacionamiento'/>
+                <p>{ estacionamiento }</p>
+            </li>
+            <li> 
+               <img src={ imagenesIconos[0].node.publicURL } alt='icono habitaciones'/>
+                <p>{ habitaciones }</p>
+            </li>
+        </ListadoIconos>
+    );
+}
--- src/styles/CommonComponents.js
+++ src/styles/CommonComponents.js
@@ -163,3 +163,18 @@ export const Contenido = styled.div`
         color: #75AB00;
     }
 `;
+
+export const ListadoIconos = styled.ul`
+    display: flex;
+    justify-content: space-between;
+    flex: 1;
+    max-width: 500px;
+    margin: 3rem auto 0 auto;
+
+    li {
+        display: flex;
+        img {
+            margin-right: 1rem;
+        }
+    }
+`;
