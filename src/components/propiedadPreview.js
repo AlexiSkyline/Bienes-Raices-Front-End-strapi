25b0bc3cfb01ad33796a8d3044531fbe6ced30b6
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-11-27 21:41:57 -0600

    we create the PropiedadPreview component

--- src/components/propiedadPreview.js
+++ src/components/propiedadPreview.js
@@ -1,1 +1,33 @@
+import React from 'react';
+import Image from 'gatsby-image';
+import urlSlug from 'url-slug';
 
+import { Boton, Card, Contenido } from '../styles/CommonComponents';
+
+export const PropiedadPreview = ({ propiedad }) => {
+
+    const { nombre,   imagen, wc, estacionamiento, habitaciones, precio } = propiedad;
+
+    return (
+        <Card>
+            <Image
+                fluid={imagen.sharp.fluid}
+            />
+            <Contenido>
+                <h3>{nombre}</h3>
+
+                <p className="precio">$ {precio} </p>
+
+                <Iconos 
+                    wc={wc}
+                    estacionamiento={estacionamiento}
+                    habitaciones={habitaciones}
+                />
+                <Boton to={ urlSlug( nombre ) }>
+                    Visitar Propiedad
+                </Boton>
+
+            </Contenido>
+        </Card>
+    );
+}
