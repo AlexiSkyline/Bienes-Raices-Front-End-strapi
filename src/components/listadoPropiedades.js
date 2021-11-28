124773881c8487183ad63cdc2458810c5dac5e57
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-11-28 16:49:34 -0600

    we added the function to filter the department types in the index

--- src/components/listadoPropiedades.js
+++ src/components/listadoPropiedades.js
@@ -4,24 +4,36 @@ import { usePropiedades } from '../hooks/usePropiedades';
 import { Propiedad, ULPropiedades } from '../styles/CommonComponents';
 import { PropiedadPreview } from './propiedadPreview';
 
+import { useFiltro } from '../hooks/useFiltro';
+
 export const ListadoPropiedades = () => {
 
     const resultado = usePropiedades();
-    const [ propiedades, setPropiedades ] = useState([]);
+    const [ propiedades ] = useState(resultado);
+    const [ filtradas, setFiltradas ] = useState([]);
+
+    const { categoria, FiltroUI } = useFiltro();
 
     useEffect(() => {
-        setPropiedades( resultado );
-    }, [input])
+        if( categoria ) {
+            const filtro = propiedades.filter( propiedad => propiedad.categoria.nombre === categoria );
+            setFiltradas( filtro );
+        } else {
+            setFiltradas( propiedades );
+        }
+    }, [categoria]);
 
     return (
         <Propiedad>
             <h2>Nuestras Propiedades</h2>
 
+            { FiltroUI() }
+
             <ULPropiedades>
                 { filtradas.map( propiedad => (
                     <PropiedadPreview 
-                        key={propiedad.id}
-                        propiedad={propiedad}
+                        key={ propiedad.id }
+                        propiedad={ propiedad }
                     />
                 ))}
             </ULPropiedades>
